c67a91a4b317890f79280ae70f08b4a07629d58b
Leonid Vlasov <leoniv.vlasov@gmail.com>
2018-04-27 20:56:35 +0700

    init

---
+++ README.md
@@ -0,0 +1,44 @@
+[![Build Status](https://travis-ci.org/leoniv/brush-sdbl.svg?branch=master)](https://travis-ci.org/leoniv/brush-sdbl)
+![Downloads](https://img.shields.io/npm/dm/brush-sdbl.svg)
+![Version](https://img.shields.io/npm/v/brush-sdbl.svg)
+
+# brush-sdbl
+
+[1C:Enterprise](http://1c.ru) query language(aka *sdbl*) brush module for
+[SyntaxHighlighter](https://github.com/syntaxhighlighter/syntaxhighlighter).
+
+## Regards!
+
+ and thanks to developers of projects:
+
+ - [SyntaxHighlighter](https://github.com/syntaxhighlighter)
+ - [xDrivenDevelopment/1c-syntax](https://github.com/xDrivenDevelopment/1c-syntax)
+
+## Usage
+
+### For [SyntaxHighlighter v3](http://alexgorbatchev.com/SyntaxHighlighter/)
+
+Use pure js file [brush-sdbl-v3.js](brush.v3/brush-sdbl-v3.js)
+
+Demo [index.html](brush.v3/index.html). For looking clone and open in your
+favorite web browser:
+
+    $git clone https://github.com/leoniv/brush-sdbl
+    $cd brush-sdbl
+    $firefox brush.v3/index.html
+
+### For SyntaxHighlighter v4
+
+Please see [Building Instructions](https://github.com/syntaxhighlighter/syntaxhighlighter/wiki/Building)
+on the [SyntaxHighlighter Wiki](https://github.com/syntaxhighlighter/syntaxhighlighter/wiki) for details.
+
+## Troubles
+
+1. Regexp \b word boundary does not work for non ASCII characters in browser
+and some keywords highlights not correctly.
+
+2. Errors occurred while [brash.js](branch.js) building.
+
+## License
+
+MIT
---
+++ brush.js
@@ -0,0 +1,67 @@
+var BrushBase = require('brush-base');
+var regexLib = require('syntaxhighlighter-regex').commonRegExp;
+
+function Brush() {
+ var keywords = 'КонецПроцедуры EndProcedure КонецФункции EndFunction'+
+                ' Прервать Break Продолжить Continue' +
+                ' Возврат Return Если If' +
+                ' Иначе Else ИначеЕсли ElsIf' +
+                ' Тогда Then КонецЕсли EndIf' +
+                ' Попытка Try Исключение Except' +
+                ' КонецПопытки EndTry Raise ВызватьИсключение' +
+                ' Пока While Для For' +
+                ' Каждого Each Из In' +
+                ' По To Цикл Do' +
+                ' КонецЦикла EndDo НЕ NOT' +
+                ' И AND ИЛИ OR' +
+                ' Новый New Процедура Procedure' +
+                ' Функция Function Перем Var' +
+                ' Экспорт Export Знач Val'
+
+  var builds = 'Неопределено Undefined Истина True Ложь False NULL'
+
+  // \b - word boundary doesn't work with utf-8
+  this.getKeywordsUTF8 = function(str){
+    const results = str
+      .replace(/^\s+|\s+$/g, '')
+      .replace(/\s+/g, '|');
+
+    const _b = '[^a-zA-Zа-яА-Я\.#&\(\)]'
+    return "(^|"+_b+"|=)?(?:"+results+")(?:"+_b+"|;|$)"
+  }
+
+  this.regexList = [
+    {
+      regex: regexLib.singleLineCComments,
+      css: 'comments'
+    },
+    {
+      regex: /("|^\s*\|)((?!\"\").)*?(\"|$)/gm,
+      css: 'string'
+    },
+    {
+      regex: /^\s*(#|&).+/gm,
+      css: 'preprocessor'
+    },
+    {
+      regex: /'.+'/g,
+      css: 'value'
+    },
+    {
+      regex: /-?\b[\d\.]+\b/g,
+      css: 'value'
+    },
+    {
+      regex: new RegExp(this.getKeywordsUTF8(keywords), 'gmi'),
+      css: 'keyword bold'
+    },
+    {
+      regex: new RegExp(this.getKeywordsUTF8(builds), 'gmi'),
+      css: 'value bold'
+    }
+    ];
+};
+
+Brush.prototype = new BrushBase();
+Brush.aliases = ['bsl', 'oscript'];
+module.exports = Brush;
